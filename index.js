4208cc4a0c15ee87efb27bc189a66de7df482a8c
nextechnexus <nextechnexus.team@gmail.com>
2024-01-20 16:53:55 +0100

    commit

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const colors = require("colors")
 const connectDB = require('./config/db')
 const app = express()
 const port  = process.env.PORT||3000
+const cookieParser = require('cookie-parser')
+let cors = require("cors")
 
 
+app.use(cookieParser())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
