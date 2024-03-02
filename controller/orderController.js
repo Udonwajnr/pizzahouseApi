30b2cb002d9e2c023ac101e9b16932e9e70f5c52
nextechnexus <nextechnexus.team@gmail.com>
2024-03-02 21:22:09 +0100

    commit

--- controller/orderController.js
+++ controller/orderController.js
@@ -7,11 +7,17 @@ const getAllOrders = asyncHandler(async(req,res)=>{
 })
 
 const createOrders = asyncHandler(async(req,res)=>{
+    const {user,menu,totalPrice} = req.body
+
+    const meal = menu.map(item => ({
+    productId: item._id,
+    quantity: item.quantity || 1, // Assuming a default quantity of 1 if not provided
+    }));
+
     const order = await Orders.create({
-        user:req.body.user,
-        menu:req.body.menu,
-        totalPrice:req.body.totalPrice
+        user,meal,totalPrice
     })
+    
     return res.status(200).json(order)
 })
 
